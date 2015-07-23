e3b27b309fa1ade9c3683704b5c088f3a612630f
miedzinski <dominik@mdzn.pl>
2015-07-23 11:34:34 +0200

    added net message handling

--- demoinfogo/demodump.py
+++ demoinfogo/demodump.py
@@ -11,6 +11,50 @@ class DemoDump(object):
     def __init__(self, demo):
         self.demo = DemoFile(demo)
 
+        self.NET_MSG = {
+            net_NOP: [],
+            net_Disconnect: [],  # disconnect, last message in connection
+            net_File: [],  # file transmission message request/deny
+            net_Tick: [],  # s->c world tick, c->s ack world tick
+            net_StringCmd: [],  # a string command
+            net_SetConVar: [],  # sends one/multiple convar/userinfo settings
+            net_SignonState: [],  # signals or acks current signon state
+        }
+        self.SVC_MSG = {
+            svc_ServerInfo: [],  # first message from server about game; map etc
+            svc_SendTable: [],  # sends a sendtable description for a game class
+            svc_ClassInfo: [],  # Info about classes (first byte is a CLASSINFO_ define).
+            svc_SetPause: [],  # tells client if server paused or unpaused
+            svc_CreateStringTable: [],  # inits shared string tables
+            svc_UpdateStringTable: [],  # updates a string table
+            svc_VoiceInit: [],  # inits used voice codecs & quality
+            svc_VoiceData: [],  # Voicestream data from the server
+            svc_Print: [],  # print text to console
+            svc_Sounds: [],  # starts playing sound
+            svc_SetView: [],  # sets entity as point of view
+            svc_FixAngle: [],  # sets/corrects players viewangle
+            svc_CrosshairAngle: [],  # adjusts crosshair in auto aim mode to lock on traget
+            svc_BSPDecal: [],  # add a static decal to the world BSP
+            svc_UserMessage: [],  # a game specific message
+            svc_GameEvent: [],  # global game event fired
+            svc_PacketEntities: [],  # non-delta compressed entities
+            svc_TempEntities: [],  # non-reliable event object
+            svc_Prefetch: [],  # only sound indices for now
+            svc_Menu: [],  # display a menu from a plugin
+            svc_GameEventList: [],  # list of known games events and fields
+            svc_GetCvarValue: [],  # Server wants to know the value of a cvar on the client
+        }
+
+    def register_on_netmsg(self, msg, callback):
+        if msg not in self.NET_MSG:
+            raise DemoError('Net message not found')
+        self.NET_MSG[msg].append(callback)
+
+    def register_on_svcmsg(self, msg, callback):
+        if msg not in self.SVC_MSG:
+            raise DemoError('Server message not found')
+        self.SVC_MSG[msg].append(callback)
+
     def do_dump(self):
         demo_finished = False
 
@@ -50,8 +94,12 @@ class DemoDump(object):
             cmd, index = self._read_int32(buf, index)
             size, index = self._read_int32(buf, index)
             data = buf[index: index + size]
-            # call events
-            print(cmd)
+            if cmd in self.NET_MSG:
+                for callback in self.NET_MSG[cmd]:
+                    callback(cmd, data)
+            if cmd in self.SVC_MSG:
+                for callback in self.SVC_MSG[cmd]:
+                    callback(cmd, data)
             index += size
 
     def _read_int32(self, buf, index):
